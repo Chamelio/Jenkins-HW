5bf4bbe3c7e551a454d05baa758060a45d28782f
Chamelio <bhull009@gmail.com>
2026-03-28 00:05:08 -0400

    second commit

--- main.tf
+++ main.tf
@@ -20,3 +20,9 @@ resource "aws_s3_object" "object-png" {
   key    = "Armageddon-Proof/passing.png"
   source = "${path.module}/Armageddon-Proof/passing.png"
 }
+
+resource "aws_s3_object" "object-png" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "Armageddon-Proof/lab-evidence.png"
+  source = "${path.module}/Armageddon-Proof/lab-evidence.png"
+}
